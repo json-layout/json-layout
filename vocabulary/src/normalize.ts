cea334b99f7079ba6b17c8bd182f02d40107c925
Alban Mouton <alban.mouton@gmail.com>
2023-08-02 11:10:10 +0200

    chore: better layout validation

--- vocabulary/src/normalize.ts
+++ vocabulary/src/normalize.ts
@@ -2,7 +2,7 @@ import {
   validateLayoutKeyword, isComponentName, isPartialCompObject, isChildren,
   type LayoutKeyword, type PartialCompObject, isResponsive, isReadWrite
 } from './layout-keyword'
-import { validateNormalizedLayout, type NormalizedLayout, type NormalizedResponsive, type CompObject } from '.'
+import { validateNormalizedLayout, normalizedLayoutKeywordSchema, type NormalizedLayout, type NormalizedResponsive, type CompObject } from '.'
 
 export interface SchemaFragment {
   layout?: LayoutKeyword
@@ -33,6 +33,17 @@ function getPartialCompObject (layoutKeyword: LayoutKeyword): PartialCompObject 
 
 function getCompObject (layoutKeyword: LayoutKeyword, defaultCompObject: CompObject): CompObject {
   const partial = getPartialCompObject(layoutKeyword)
+  if (!partial) return defaultCompObject
+  if (partial.comp && defaultCompObject.comp !== partial.comp) {
+    const compProperties = normalizedLayoutKeywordSchema.$defs[partial.comp]?.properties
+    const mergedCompObject: CompObject = {} as CompObject
+    if (typeof compProperties === 'object') {
+      for (const key of Object.keys(compProperties)) {
+        if (key in defaultCompObject) mergedCompObject[key as keyof CompObject] = defaultCompObject[key as keyof CompObject]
+      }
+    }
+    return Object.assign(mergedCompObject, partial) as CompObject
+  }
   return Object.assign({}, defaultCompObject, partial) as CompObject
 }
 
@@ -65,12 +76,14 @@ function getNormalizedLayout (layoutKeyword: LayoutKeyword, defaultCompObject: C
 export function normalizeLayoutFragment (schemaFragment: SchemaFragment, schemaPath: string): NormalizedLayout {
   const layoutKeyword = schemaFragment.layout ?? {}
   if (!validateLayoutKeyword(layoutKeyword)) {
+    console.log(`layout keyword validation errors at path ${schemaPath}`, validateNormalizedLayout.errors)
     throw new Error(`invalid layout keyword at path ${schemaPath}`, { cause: validateLayoutKeyword.errors })
   }
   const defaultCompObject = getDefaultCompObject(schemaFragment, schemaPath)
   const normalizedLayout = getNormalizedLayout(layoutKeyword, defaultCompObject)
   if (!validateNormalizedLayout(normalizedLayout)) {
-    throw new Error('invalid layout', { cause: validateNormalizedLayout.errors })
+    console.log(`normalized layout validation errors at path ${schemaPath}`, validateNormalizedLayout.errors)
+    throw new Error(`invalid layout at path ${schemaPath}`, { cause: validateNormalizedLayout.errors })
   }
   return normalizedLayout
 }
