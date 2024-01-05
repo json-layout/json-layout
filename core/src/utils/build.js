4ee0ab730c6249c9e63d861f4dac76396acc2e12
Alban Mouton <alban.mouton@gmail.com>
2024-01-05 17:08:56 +0100

    feat: expose list of commonjs deps for bundlers

---
+++ core/src/utils/build.js
@@ -0,0 +1,10 @@
+// these dependencies are not pure ESM, users will probably need to inform their bundler to treat them as commonjs
+export const commonjsDeps = [
+  'ajv',
+  'ajv-formats',
+  'ajv-formats/dist/formats.js',
+  'ajv-i18n',
+  'ajv-errors',
+  'markdown-it',
+  'debug'
+]
