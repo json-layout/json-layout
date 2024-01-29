297176c88b5d25b70059ea66489bb9c11119e71d
Alban Mouton <alban.mouton@gmail.com>
2024-01-29 20:45:40 +0100

    chore: expose commonjsDepsPaths for build systems

--- core/src/utils/build.js
+++ core/src/utils/build.js
@@ -1,5 +1,8 @@
 // these dependencies are not pure ESM, users will probably need to inform their bundler to treat them as commonjs
+
+/** @type {string[]} */
 export const commonjsDeps = [
+  'ajv',
   'ajv/dist/2019.js',
   'ajv-formats',
   'ajv-formats/dist/formats.js',
@@ -7,4 +10,16 @@ export const commonjsDeps = [
   'ajv-errors',
   'markdown-it',
   'debug'
+]
+
+/** @type {string[][]} */
+export const commonjsDepsPaths = [
+  ['@json-layout/core', 'ajv'],
+  ['@json-layout/core', 'ajv/dist/2019.js'],
+  ['@json-layout/core', 'ajv-formats'],
+  ['@json-layout/core', 'ajv-formats/dist/formats.js'],
+  ['@json-layout/core', 'ajv-i18n'],
+  ['@json-layout/core', 'ajv-errors'],
+  ['@json-layout/core', 'markdown-it'],
+  ['@json-layout/core', 'debug']
 ]
