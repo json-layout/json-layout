b859f3c6a8caa40138c53b2f48f67433c5c78c7d
Alban Mouton <alban.mouton@gmail.com>
2024-03-08 13:49:35 +0100

    docs: add example for readOnlyPropertiesMode option

--- examples/src/composite/index.js
+++ examples/src/composite/index.js
@@ -4,13 +4,14 @@ import tabs from './tabs.js'
 import verticalTabs from './vertical-tabs.js'
 import expansionPanels from './expansion-panels.js'
 import complexChildren from './complex-children.js'
+import readonlyChildren from './readonly-children.js'
 
 /** @type {import('../types.js').JSONLayoutExamplesCategory} */
 const category = {
   title: 'Composite components',
   id: 'composite',
   description: 'There are a few components available to structure the layout and wrap fields : sections, tabs, etc.',
-  examples: [section, sectionParent, tabs, verticalTabs, expansionPanels, complexChildren]
+  examples: [section, sectionParent, tabs, verticalTabs, expansionPanels, complexChildren, readonlyChildren]
 }
 
 export default category
---
+++ examples/src/composite/readonly-children.js
@@ -0,0 +1,87 @@
+/** @type {import("../types.js").JSONLayoutExample } */
+const example = {
+  title: 'Read-only children',
+  id: 'readonly-children',
+  description: 'The standard JSON Schema attribute `readOnly` can have multiple effect on children of a section depending on the `readOnlyPropertiesMode` option.',
+  schema: {
+    type: 'object',
+    properties: {
+      section1: {
+        type: 'object',
+        title: 'Section with visible read-only children',
+        layout: {
+          options: {
+            readOnlyPropertiesMode: 'show'
+          }
+        },
+        properties: {
+          str1: {
+            type: 'string',
+            title: 'String 1'
+          },
+          str2: {
+            type: 'string',
+            title: 'String 2',
+            readOnly: true
+          }
+        }
+      },
+      section2: {
+        type: 'object',
+        title: 'Section with hidden read-only children',
+        layout: {
+          options: {
+            readOnlyPropertiesMode: 'hide'
+          }
+        },
+        properties: {
+          str1: {
+            type: 'string',
+            title: 'String 1'
+          },
+          str2: {
+            type: 'string',
+            title: 'String 2',
+            readOnly: true
+          }
+        }
+      },
+      section3: {
+        type: 'object',
+        title: 'Section with read-only children removed from the data',
+        layout: {
+          options: {
+            readOnlyPropertiesMode: 'remove'
+          }
+        },
+        properties: {
+          str1: {
+            type: 'string',
+            title: 'String 1'
+          },
+          str2: {
+            type: 'string',
+            title: 'String 2',
+            readOnly: true
+          }
+        }
+      }
+    }
+  },
+  data: {
+    section1: {
+      str1: 'String 1 value',
+      str2: 'String 2 value'
+    },
+    section2: {
+      str1: 'String 1 value',
+      str2: 'String 2 value'
+    },
+    section3: {
+      str1: 'String 1 value',
+      str2: 'String 2 value'
+    }
+  }
+}
+
+export default example
