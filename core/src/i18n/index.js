0f5a8abe8e1f527440661ab74d1e5b4f2a1c6c63
Rob <rob@unity-x.nl>
2024-06-27 15:58:21 +0200

    feat(lang): add Dutch language

--- core/src/i18n/index.js
+++ core/src/i18n/index.js
@@ -1,8 +1,10 @@
 import en from './en.js'
 import fr from './fr.js'
+import nl from './nl.js'
 
 /** @type {Record<string, import('./types.js').LocaleMessages>} */
 export default {
   en,
-  fr
+  fr,
+  nl
 }
